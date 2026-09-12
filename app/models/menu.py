ed9bd6211f42924ea4a02c8d28379c3ed70597ff
ADEDANCE <uthmanadedance@gmail.com>
2026-09-12 18:07:24 +0100

    Refractor:adjust the menu item

--- app/models/menu.py
+++ app/models/menu.py
@@ -1,10 +1,7 @@
-from sqlalchemy import String,Integer,Boolean,Text
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
 
+from app.database.base import Base
 
-
-class Base(DeclarativeBase):
-    pass
 
 class MenuItem(Base):
     __tablename__ = "menu_items"
