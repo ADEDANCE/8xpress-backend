a67892ccae70ee3c02996022086b8ffee62ce983
ADEDANCE <uthmanadedance@gmail.com>
2026-09-13 22:51:20 +0100

    Feat: Create get current user

---
+++ app/dependencies.py
@@ -0,0 +1,38 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.auth import verify_token
+from app.database.database import AsyncSessionLocal
+from app.models.user import User
+
+# "For endpoints that use this dependency, expect a Bearer token
+security = HTTPBearer()
+
+# creates a database session for the request
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
+
+
+async def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    db: AsyncSession = Depends(get_db),
+) -> User:
+
+    user_id = verify_token(credentials)
+
+    result = await db.execute(
+        select(User).where(User.id == user_id)
+    )
+
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    return user
