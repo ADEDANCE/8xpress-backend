1c25f8ccfeb9f52d498b61397e3261d7a11b9bc6
ADEDANCE <uthmanadedance@gmail.com>
2026-09-13 22:53:14 +0100

    load secret key from env file

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
 #     DATABASE_URL = DATABASE_URL.replace(
