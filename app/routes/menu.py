22ffbb93df301858fe246b144bfa47c6e94d2c0a
ADEDANCE <uthmanadedance@gmail.com>
2026-09-13 23:24:41 +0100

    Feat: Create get menu api

--- app/routes/menu.py
+++ app/routes/menu.py
@@ -1,11 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.dependencies import get_db
+from app.models.menu import MenuItem
+from app.schemas.menu import MenuItemResponse
+
 
 router = APIRouter()
 
 
-@router.get("/{item_id}")
-def get_menu_item(item_id: int):
-    return {
-        "item_id": item_id,
-        "message": "Menu item requested"
-    }
+@router.get("/", response_model=list[MenuItemResponse])
+async def get_menu(
+    # gives the endpoint a database session.
+    db: AsyncSession = Depends(get_db),
+):
+# Get MenuItem records from the database
+    result = await db.execute(
+        select(MenuItem).where(MenuItem.is_available == True)
+    )
+# converts the database result into a list of MenuItem objects.
+    menu_items = result.scalars().all()
+
+# sends them back to the customer
+    return menu_items
