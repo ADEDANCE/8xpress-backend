8ea585b9a37354c112239402a7b2cdc5c5b1caad
ADEDANCE <uthmanadedance@gmail.com>
2026-09-14 12:11:59 +0100

    Feat:Create menu update schema

--- app/schemas/menu.py
+++ app/schemas/menu.py
@@ -8,6 +8,15 @@ class MenuItemCreate(BaseModel):
     image_url: str
 
 
+class MenuItemUpdate(BaseModel):
+    name: str
+    description: str
+    price: float
+    image_url: str
+    is_available: bool
+
+
+
 class MenuItemResponse(BaseModel):
     id: int
     name: str
