b95aace5aa95d508c63da906f04b7edc835dec80
ADEDANCE <uthmanadedance@gmail.com>
2026-09-14 11:40:57 +0100

    Refractor:make authorise user admin

--- app/dependencies.py
+++ app/dependencies.py
@@ -36,3 +36,17 @@ async def get_current_user(
         )
 
     return user
+
+
+
+async def require_admin(
+    current_user: User = Depends(get_current_user),
+) -> User:
+
+     if current_user.role != "admin":
+        raise HTTPException(
+            status_code=403,
+            detail="Admin access required"
+        )
+
+        return current_user
